19821013e05ae2f786c4ba6029c7f723af8a2fa7
Alexander Razzhivin <admin@httpbots.com>
2017-04-21 20:58:47 +0300

    reset and base scss file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
 var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
+var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync');
+var pxtorem = require('postcss-pxtorem');
 
 var appDir = 'app';
 
 gulp.task('sass', function() {                  // Создаем таск Sass
+
+  var processors = [
+       autoprefixer('> 5%', 'ie >= 9'),
+       pxtorem({
+         rootValue: 10,
+          propList: ['*']
+       })
+   ];
+
   return gulp.src(appDir+'/scss/**/*.+(scss|sass)')     // Берем источник
       .pipe(sourcemaps.init())                  // Инициализируем sourcemap для отладки
       .pipe(sass({
@@ -14,7 +25,7 @@ gulp.task('sass', function() {                  // Создаем таск Sass
           includePaths: ['node_modules/susy/sass']
       })
       .on('error', sass.logError))            // Преобразуем Sass в CSS посредством gulp-sass
-      .pipe(prefix("> 5%", 'ie >= 9'))
+      .pipe(postcss(processors))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest( appDir+'/css') )         // Выгружаем результата в папку app/css
       .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
