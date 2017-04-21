bc5895ef5d538d71bb9a8985aa2114e7f4a16975
Alexander Razzhivin <admin@httpbots.com>
2017-04-21 22:43:46 +0300

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var autoprefixer = require('autoprefixer');
 var browserSync = require('browser-sync');
 var pxtorem = require('postcss-pxtorem');
 
-var appDir = 'app';
+var srcDir = 'src';
 
 gulp.task('sass', function() {                  // Создаем таск Sass
 
@@ -18,7 +18,7 @@ gulp.task('sass', function() {                  // Создаем таск Sass
        })
    ];
 
-  return gulp.src(appDir+'/scss/**/*.+(scss|sass)')     // Берем источник
+  return gulp.src(srcDir+'/scss/**/*.+(scss|sass)')     // Берем источник
       .pipe(sourcemaps.init())                  // Инициализируем sourcemap для отладки
       .pipe(sass({
           outputStyle: 'nested',                // Возможные значения: nested, expanded, compact, compressed
@@ -27,14 +27,14 @@ gulp.task('sass', function() {                  // Создаем таск Sass
       .on('error', sass.logError))            // Преобразуем Sass в CSS посредством gulp-sass
       .pipe(postcss(processors))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest( appDir+'/css') )         // Выгружаем результата в папку app/css
+      .pipe(gulp.dest( srcDir+'/css') )         // Выгружаем результата в папку app/css
       .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
 gulp.task('browser-sync', function() {  // Создаем таск browser-sync
     browserSync({                       // Выполняем browser Sync
         server: {                       // Определяем параметры сервера
-            baseDir: appDir             // Директория для сервера - app
+            baseDir: srcDir             // Директория для сервера - app
         },
         notify: false // Отключаем уведомления
     });
@@ -42,9 +42,9 @@ gulp.task('browser-sync', function() {  // Создаем таск browser-sync
 
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
-    gulp.watch(appDir+'/scss/**/*.+(scss|sass)', ['sass']); // Наблюдение за sass файлами
-    gulp.watch(appDir+'/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-    gulp.watch(appDir+'/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
+    gulp.watch(srcDir+'/scss/**/*.+(scss|sass)', ['sass']); // Наблюдение за sass файлами
+    gulp.watch(srcDir+'/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+    gulp.watch(srcDir+'/js/**/*.js', browserSync.reload); // Наблюдение за JS файлами в папке js
 
 });
 
